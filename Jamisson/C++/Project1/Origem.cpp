bee1ec58c735caebc165191e0834822f7d6f5a29
JJader <jjader03@gmail.com>
2018-05-13 01:51:57 -0300

    Entrada no usuário no programa de c++

--- Jamisson/C++/Project1/Origem.cpp
+++ Jamisson/C++/Project1/Origem.cpp
@@ -10,8 +10,8 @@ using namespace cv;
 int main(void) {
 	int a, b;
 	char op;
-	Mat source;
 	Mat img;
+	Mat borda;
 	VideoCapture cam(0);
 	namedWindow("WebCam");
 	namedWindow("Borda");
@@ -19,10 +19,10 @@ int main(void) {
 	b = 100;
 	while (true)
 	{
-		cam.read(source);
-		Canny(source, img, a, b);
-		imshow("WebCam", source);
-		imshow("Borda", img);
+		cam.read(img);
+		Canny(img, borda, a, b);
+		imshow("WebCam", img);
+		imshow("Borda", borda);
 		waitKey(25);
 		if (_kbhit()) {cin >> op; if (op == 'a') { cin >> a; }else {if (op == 'b') { cin >> b; }}}
 	}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Deteccao_Borda
+﻿# Deteccao_Borda
 Trabalho de calculo; Programa para detecção de borda; Engenharia de Computação 1
+
+--> Thiago : arquivos desenvolvidos pelo aluno thiago
+	* Scilab: imagem estática
+	* Scilab2: código fonte do programa de detecção de borda pela webcam no scilab
+
+--> Jamisson: arquivos desenvolvidos pelo aluno jamisson
+	*C++ código fonte do programa de detecção de borda pela webcam em c++
+	*tamanho código fonte para detecção do tamanho da imagem em pixel.
+
+--> apresentação: slides da apresentação em sala de aula
