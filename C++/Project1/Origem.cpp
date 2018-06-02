d403d9196cabd0106309d19e702704aa2d5ade6e
JJader <jjader03@gmail.com>
2018-06-02 03:46:46 -0300

    3 - melhorias no programa c++
    inserindo entradas de salvar imagen, help, talvez conseguil pegar o tamanho do objeto;

--- C++/Project1/Origem.cpp
+++ C++/Project1/Origem.cpp
@@ -5,11 +5,19 @@
 #include <stdlib.h>
 #include <conio.h>
 
+
 using namespace std;
 using namespace cv;
 
+
+
+
+
+
 int main(void) {
-	int a, b, aux1 = 1, aux2 = 1, aux3 = 1, tam[2];
+	int a, b, aux1 = 1, aux2 = 1, aux3 = 1;
+	int name = 0, nome = 0, tam[2];
+	int col, row;
 	string op;
 	Mat img;
 	Mat borda;
@@ -20,35 +28,62 @@ int main(void) {
 	b = 100;
 	while (aux1 == 1)
 	{
-		aux3 = 1;
+		
 		cam.read(img);
 		Canny(img, borda, a, b);
-		if (aux2 == 1){
-			int col = borda.cols;
-			int row = borda.rows;
-			aux2 += 1;
-		}
-		/*
-		for (int y = 50; y <= 430 ; y++){
-			for (int x = 100; x <= 540; x++){
-				if (borda.at<int>(x, y) == 255 && aux3 == 1) { tam[0] = y; aux3 = 2; }
-				else if (borda.at<int>(x, y) == 255) { tam[1] = y; }
-			}
-		}
-		cout << tam[1] - tam[0] << endl;
-		*/
+		
 		putText(img, "Ola  mundo", { 0,475 }, CV_FONT_HERSHEY_SIMPLEX, 1,{ 0,255,0 }, 3, cv::LINE_AA);
 		rectangle(img, { 100,50 }, { 540,430 }, { 0,255,0 }, 2, cv::LINE_AA,0);
 		imshow("WebCam", img);
 		imshow("Borda", borda);
 		waitKey(25);
+
+
 		if (_kbhit()) {
 			cin >> op;
-			if (op == "a") { cin >> a; }
+				if (op == "a") { cin >> a; }
 				else if (op == "b") { cin >> b; }
 				else if (op == "abort") { aux1 +=1;}
-		}	
+				else if (op == "saveb"){
+					ostringstream con;
+					con << name;
+					imwrite("borda" + con.str() + ".jpg", borda);
+					name++;
+				}
+				else if (op == "saveim") {
+					ostringstream con;
+					con << nome;
+					imwrite("imagen" + con.str() + ".jpg", img);
+					nome++;
+				}
+				else{
+					system("cls");
+					cout << "a      --> Alterar A" << endl;
+					cout << "b      --> Alterar B" << endl;
+					cout << "saveb  --> Salvar borda" << endl;
+					cout << "saveim --> Salvar imagem" << endl;
+					cout << "abort  --> Finalizar" << endl << endl;
+				}}
 	}
-	
+	col = borda.cols;
+	row = borda.rows;
+
+	for (int y = 0; y < col; y++)
+	{
+		for (int x = 0; x < row; x++)
+		{
+			if (borda.at<uchar>(x, y) == 255 && aux3 == 1){
+				tam[0] = y;
+				aux3++;
+			}
+			else if (borda.at<uchar>(x, y) == 255) {
+				tam[1] = y;
+			}
+			printf("%d ", (borda.at<uchar>(x,y)));
+		}
+		printf(";\n");
+	}
+	printf("%d \n%d\n", tam[0], tam[1]);
 	system("pause");
+	return 0;
 }
